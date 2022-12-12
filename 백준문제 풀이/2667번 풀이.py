5e21b8db143a5d3b909ce2281e68143cd2dcfede
wjdghcks6735 <wjdghcks02@gmail.com>
2022-12-12 15:03:18 +0900

    20221212문제 2667번 오류 수정

--- 백준문제 풀이/2667번 풀이.py
+++ 백준문제 풀이/2667번 풀이.py
@@ -43,21 +43,23 @@ print(*c, sep='\n')
 """ dfs 풀이
 
 import sys
-from collecions import deque
+from collections import deque
 
 input = sys.stdin.readline
 
 N = int(input())
 
-graph = []
-result =[]
+graph = []  # 입력받을 그래프를 담을 리스트 선언
+result = []  # 결과를 담을 리스트 선언
 count = 0
 
 for _ in range(N):
-    graph.append(list(map(int, input().split())))
-    
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+    graph.append(list(map(int, input().rstrip())))
+
+# 한 점을 기준으로 (위 아래 왼쪽 오른쪽) 으로 한칸 씩 이동할 좌표 설정
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
 
 def dfs(x, y):
     global count
@@ -72,16 +74,17 @@ def dfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
             dfs(nx, ny)
-            
+
+
+# 그래프의 원소가 1일때만 dfs로 집을 방문한다.
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
             dfs(i, j)
             result.append(count)
             count = 0
-                 
-result.sort()
-
+            
+result.sort()           
 print(len(result))
 print(*result, sep='\n')
 """
