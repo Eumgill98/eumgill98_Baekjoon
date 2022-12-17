39387f713aee127f6cad2a3d2e61238bbd3cb764
wjdghcks6735 <wjdghcks02@gmail.com>
2022-12-17 17:16:32 +0900

    20221217 하노이탑 알고리즘 1문제

---
+++ 백준문제 풀이/11729번 풀이.py
@@ -0,0 +1,12 @@
+#하노이탑 알고리즘
+def hanoi(n, start, end, mid):
+    if n == 1:
+        print(start , end)
+        return
+    hanoi(n-1, start, mid, end)
+    print(start, end)
+    hanoi(n-1, mid, end, start)
+
+n = int(input())
+print((2**n)-1)
+hanoi(n, 1, 3, 2)
