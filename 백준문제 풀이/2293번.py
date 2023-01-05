7e9d360ab2b24f0428876df0375ff8f622e881da
wjdghcks6735 <wjdghcks02@gmail.com>
2023-01-05 19:51:09 +0900

    20230105

---
+++ 백준문제 풀이/2293번.py
@@ -0,0 +1,12 @@
+
+n, k = map(int, input().split())
+coin = [int(input()) for f in range(n)]
+dp = [0] * (k+1)
+dp[0] = 1
+
+for i in coin:
+    for j in range(i, k+1):
+        if j-i >= 0:
+            dp[j] += dp[j-i]
+
+print(dp[k])
