d910a2e1754c3a27d6e7f41cc8329f8c6d77d851
wjdghcks6735 <wjdghcks02@gmail.com>
2023-01-09 18:30:04 +0900

    20230109

---
+++ 백준문제 풀이/2294번 풀이.py
@@ -0,0 +1,14 @@
+
+n, k = map(int, input().split())
+coin = [int(input()) for f in range(n)]
+
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for num in coin:
+   for i in range(num, k+1):
+       dp[i] = min(dp[i],dp[i-num]+1)
+if dp[k] == 10001:
+   print(-1)
+else:
+   print(dp[k])
